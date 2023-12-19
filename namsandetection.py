f416655f4f82df0431fa7e4b38d0c36e4c74acfe
TaeHyoun Kim <kth9903@gmail.com>
2023-12-19 14:53:07 +0900

    refactor : modify logic to test image

--- namsandetection.py
+++ namsandetection.py
@@ -1,46 +1,51 @@
-import torch
 import sys
+import torch
 import os
 import cv2
 
+def load_model(model_path):
+    if not os.path.exists(model_path):
+        print(f"Model file {model_path} not found.")
+        sys.exit(1)
+    return torch.load(model_path)
+
+def process_image(model, image_path, confidence_threshold):
+    prediction = model.predict(image_path, confidence=0, overlap=30).json()
+    predictions = prediction["predictions"]
+    highest_confidence_pred = max(predictions, key=lambda x: x["confidence"]) if predictions else None
+
+    if highest_confidence_pred:
+        print(f"Highest confidence prediction: {highest_confidence_pred['confidence']}")
+
+        if highest_confidence_pred["confidence"] * 100 > confidence_threshold:
+            image = cv2.imread(image_path)
+            x, y, width, height = highest_confidence_pred["x"], highest_confidence_pred["y"], highest_confidence_pred["width"], highest_confidence_pred["height"]
+            topleft = (int(x - width / 2), int(y - height / 2))
+            bottomright = (int(x + width / 2), int(y + height / 2))
+            cv2.rectangle(image, topleft, bottomright, (0, 255, 0), 5)
+            return image
+        else:
+            print("No prediction above the confidence threshold.")
+            return None
+    else:
+        print("No predictions found.")
+        return None
+
+def display_image(image):
+    if image is not None:
+        cv2.namedWindow("Namsan Tower Detection", cv2.WINDOW_NORMAL)
+        cv2.imshow("Namsan Tower Detection", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
 model_path = "namsan.pt"
-if os.path.exists(model_path):
-    model = torch.load(model_path)
+model = load_model(model_path)
 
-if len(sys.argv) < 2:
-    print("Usage: python3 namsandetection.py <image file>")
-    sys.exit(1)
-    
-image_path = sys.argv[1]
-
+image_path = input("Enter the image file path: ")
 if not os.path.exists(image_path):
     print("Image file not found")
     sys.exit(1)
-    
-prediction = model.predict(image_path, confidence=0, overlap=30).json()
 
-predictions = prediction["predictions"]
-
-highest_confindence_pred = max(predictions, key=lambda x: x["confidence"]) if predictions else None
-
-if highest_confindence_pred:
-    print(f"Highest confidence prediction: {highest_confindence_pred['confidence']}")
-    
-image = cv2.imread(image_path)
-
-confidence_trheshold = 40
-if highest_confindence_pred and highest_confindence_pred["confidence"]*100 > confidence_trheshold:
-    x = highest_confindence_pred["x"]
-    y = highest_confindence_pred["y"]
-    width = highest_confindence_pred["width"]
-    height = highest_confindence_pred["height"]
-    
-    topleft = (int(x-width/2), int(y-height/2))
-    bottomright = (int(x+width/2), int(y+height/2))
-    
-    cv2.rectangle(image, topleft, bottomright, (0, 255, 0), 5)
-    
-cv2.namedWindow("Namsan Tower Detection", cv2.WINDOW_NORMAL)
-cv2.imshow("Namsan Tower Detection", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+confidence_threshold = 40
+image_with_prediction = process_image(model, image_path, confidence_threshold)
+display_image(image_with_prediction)
